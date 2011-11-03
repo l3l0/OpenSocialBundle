266892dc30e483fcde505f2483f2a9de13c48f3a
Leszek Prabucki <leszek.prabucki@gmail.com>
2011-11-03 19:14:23 +0100

    Added some documentation tags for files

--- DependencyInjection/l3l0OpenSocialExtension.php
+++ DependencyInjection/l3l0OpenSocialExtension.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+* This file is part of the OpenSocialBundle package.
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace l3l0\Bundle\OpenSocialBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
